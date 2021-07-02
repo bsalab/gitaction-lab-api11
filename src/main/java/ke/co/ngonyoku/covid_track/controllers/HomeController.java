f74b51800ca1cea361fdedf625d326deb4622300
Ngonyoku <ngonyoku001@gmail.com>
2021-07-02 15:09:15 +0300

    Confirmed Recovery cases added

--- src/main/java/ke/co/ngonyoku/covid_track/controllers/HomeController.java
+++ src/main/java/ke/co/ngonyoku/covid_track/controllers/HomeController.java
@@ -2,6 +2,7 @@ package ke.co.ngonyoku.covid_track.controllers;
 
 import ke.co.ngonyoku.covid_track.models.CasesConfirmedGlobally;
 import ke.co.ngonyoku.covid_track.models.DeathsConfirmedGlobally;
+import ke.co.ngonyoku.covid_track.models.RecoveriesConfirmedGlobally;
 import ke.co.ngonyoku.covid_track.services.CovidTrackerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,11 +16,16 @@ public class HomeController {
     @Autowired
     private CovidTrackerService covidTrackerService;
 
-    @GetMapping("/")
-    public String getHome(Model model) {
-        List<CasesConfirmedGlobally> casesConfirmedGloballyList = covidTrackerService.getCasesConfirmedGloballyList();
-        List<DeathsConfirmedGlobally> deathsConfirmedGloballyList = covidTrackerService.getDeathsConfirmedGloballyList();
+    public static final String APP_NAME = "Covid-Track";
 
+    @GetMapping("/")
+    public String home(Model model) {
+        List<CasesConfirmedGlobally> casesConfirmedGloballyList =
+                covidTrackerService.getCasesConfirmedGloballyList();
+        List<DeathsConfirmedGlobally> deathsConfirmedGloballyList =
+                covidTrackerService.getDeathsConfirmedGloballyList();
+        List<RecoveriesConfirmedGlobally> recoveriesConfirmedGloballyList =
+                covidTrackerService.getRecoveriesConfirmedGloballyList();
         int totalCasesReported = casesConfirmedGloballyList
                 .stream()
                 .mapToInt(
@@ -29,13 +35,24 @@ public class HomeController {
         int totalDeathsReported = deathsConfirmedGloballyList
                 .stream()
                 .mapToInt(
-                        deathsList -> deathsList.getTotalDeathsConfirmed()
+                        deathsList -> deathsList.getLatestTotalDeathsConfirmed()
                 )
                 .sum(); //Returns the Sum of all the Deaths reported
+        int totalRecoveriesReported = recoveriesConfirmedGloballyList
+                .stream()
+                .mapToInt(
+                        recoveryList -> recoveryList.getLatestTotalRecoveriesReported()
+                )
+                .sum();
 
-        model.addAttribute("confirmedCasesList", casesConfirmedGloballyList);
-        model.addAttribute("confirmedDeathsList", deathsConfirmedGloballyList);
+        model.addAttribute("appName", APP_NAME);
+
+        model.addAttribute("casesConfirmedGloballyList", casesConfirmedGloballyList);
+        model.addAttribute("deathsConfirmedGloballyList", deathsConfirmedGloballyList);
+        model.addAttribute("recoveriesConfirmedGloballyList", recoveriesConfirmedGloballyList);
+
         model.addAttribute("totalCasesReported", totalCasesReported);
+        model.addAttribute("totalRecoveriesReported", totalRecoveriesReported);
         model.addAttribute("totalDeathsReported", totalDeathsReported);
         return "index";
     }
--- src/main/java/ke/co/ngonyoku/covid_track/services/CovidTrackerService.java
+++ src/main/java/ke/co/ngonyoku/covid_track/services/CovidTrackerService.java
@@ -1,6 +1,7 @@
 package ke.co.ngonyoku.covid_track.services;
 
 import ke.co.ngonyoku.covid_track.models.CasesConfirmedGlobally;
+import ke.co.ngonyoku.covid_track.models.RecoveriesConfirmedGlobally;
 import ke.co.ngonyoku.covid_track.models.DeathsConfirmedGlobally;
 import lombok.Getter;
 import org.apache.commons.csv.CSVFormat;
@@ -23,10 +24,11 @@ import java.util.List;
 public class CovidTrackerService {
     public static final String CONFIRMED_CASES_GLOBAL_URL = "https://raw.githubusercontent.com/Ngonyoku/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
     public static final String CONFIRMED_DEATHS_GLOBAL_URL = "https://raw.githubusercontent.com/Ngonyoku/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv";
+    public static final String CONFIRMED_RECOVERED_CASES_GLOBAL = "https://raw.githubusercontent.com/Ngonyoku/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv";
 
-
-        private List<CasesConfirmedGlobally> casesConfirmedGloballyList = new ArrayList<>();
+    private List<CasesConfirmedGlobally> casesConfirmedGloballyList = new ArrayList<>();
     private List<DeathsConfirmedGlobally> deathsConfirmedGloballyList = new ArrayList<>();
+    private List<RecoveriesConfirmedGlobally> recoveriesConfirmedGloballyList = new ArrayList<>();
 
     private Iterable<CSVRecord> fetchRecordFromCSV(String URL) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient(); //Create a HttpClient
@@ -54,6 +56,7 @@ public class CovidTrackerService {
     public void fetchData() throws IOException, InterruptedException {
         List<CasesConfirmedGlobally> newCasesConfirmedGloballyList = new ArrayList<>();
         List<DeathsConfirmedGlobally> newDeathConfirmedGlobally = new ArrayList<>();
+        List<RecoveriesConfirmedGlobally> newRecoveriesConfirmedGloballyList = new ArrayList<>();
 
         /*Loop through the Confirmed Cases Records*/
         for (CSVRecord record : fetchRecordFromCSV(CONFIRMED_CASES_GLOBAL_URL)) {
@@ -87,9 +90,23 @@ public class CovidTrackerService {
             newDeathConfirmedGlobally.add(deathsConfirmedGlobally);//Populate our List
         }
 
+        /*Loop through the Confirmed Deaths Records*/
+        for (CSVRecord record : fetchRecordFromCSV(CONFIRMED_RECOVERED_CASES_GLOBAL)) {
+            int lastColumn = Integer.parseInt(record.get(record.size() - 1));//Get recent updates in the last column
+            RecoveriesConfirmedGlobally recoveriesConfirmedGlobally =
+                    new RecoveriesConfirmedGlobally(
+                            record.get("Country/Region"), //CountryOrRegion
+                            record.get("Province/State"), //ProvinceOrState
+                            record.get("Lat"), //Latitude
+                            record.get("Long"), //Longitude
+                            lastColumn //Latest Cases Reported
+                    );
+            newRecoveriesConfirmedGloballyList.add(recoveriesConfirmedGlobally);
+        }
+
         /*Update the current list of cases*/
         this.casesConfirmedGloballyList = newCasesConfirmedGloballyList;
         this.deathsConfirmedGloballyList = newDeathConfirmedGlobally;
-
+        this.recoveriesConfirmedGloballyList = newRecoveriesConfirmedGloballyList;
     }
 }
